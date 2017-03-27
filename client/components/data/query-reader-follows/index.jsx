4ff001574e9544415a1aa184d39dfaeb9d8bb84c
Jake F - @samouri <samouri@users.noreply.github.com>
2017-03-26 21:58:19 -0400

    added query component + selector for follows

---
+++ client/components/data/query-reader-follows/index.jsx
@@ -0,0 +1,26 @@
+/**
+ * External dependencies
+ */
+import { Component } from 'react';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { requestFollows } from 'state/reader/follows/actions';
+
+class QueryReaderFollows extends Component {
+	componentWillMount() {
+		this.request( this.props );
+		this.props.requestFollows;
+	}
+
+	render() {
+		return null;
+	}
+}
+
+export default connect(
+	null,
+	{ requestFollows },
+)( QueryReaderFollows );
