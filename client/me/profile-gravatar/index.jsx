fd1ee9276a72458cdab5c3bd2829730a6b5fd24e
Valerie <valerie@automattic.com>
2017-03-28 09:42:48 -0400

    Me: Remove Gravatar link from profile-gravatar

--- client/me/profile-gravatar/index.jsx
+++ client/me/profile-gravatar/index.jsx
@@ -11,6 +11,7 @@ import { localize } from 'i18n-calypso';
 import Animate from 'components/animate';
 import Gravatar from 'components/gravatar';
 import eventRecorder from 'me/event-recorder';
+import { isEnabled } from 'config';
 
 const debug = debugFactory( 'calypso:me:sidebar-gravatar' );
 
@@ -25,6 +26,18 @@ const ProfileGravatar = React.createClass( {
 
 	render() {
 		const profileURL = `//gravatar.com/${ this.props.user.username }`;
+
+		if ( isEnabled( 'me/edit-gravatar' ) ) {
+			return (
+				<div className="profile-gravatar">
+					<Gravatar user={ this.props.user } size={ 150 } imgSize={ 400 } />
+					<h2 className="profile-gravatar__user-display-name">{ this.props.user.display_name }</h2>
+					<div className="profile-gravatar__user-secondary-info">
+						<a href={ profileURL } target="_blank" rel="noopener noreferrer">@{ this.props.user.username }</a>
+					</div>
+				</div>
+			);
+		}
 
 		return (
 			<div className="profile-gravatar">
