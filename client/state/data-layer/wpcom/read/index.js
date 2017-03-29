bdc6346b0faed65ef41ac0eece46fb8a0f5864f1
Jake F - @samouri <samouri@users.noreply.github.com>
2017-03-29 15:05:06 -0400

    add middleware to be used

--- client/state/data-layer/wpcom/read/index.js
+++ client/state/data-layer/wpcom/read/index.js
@@ -4,8 +4,10 @@
 import { mergeHandlers } from 'state/data-layer/utils';
 import teams from './teams';
 import tags from './tags';
+import feed from './feed';
 
 export default mergeHandlers(
 	teams,
 	tags,
+	feed,
 );
